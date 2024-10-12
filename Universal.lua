c2c493c1120ecd5dbb74cdc17e48ad181c20d554
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-12 11:18:30 -0400

    update

--- Universal.lua
+++ Universal.lua
@@ -6339,6 +6339,6 @@ task.spawn(function()
 	end
 
 	if shared.deffont and shared.deffont ~= Enum.Font.Arial then 
-		infonotif("Vape: Font Loaded.", "Font changed to "..string.gsub(tostring(shared.deffont), "Enum.Font", "")..".", 5)
+		infonotif("Vape: Font Loaded.", "Font changed to "..string.gsub(tostring(shared.deffont), "Enum.Font" and ".", "")..".", 5)
 	end
 end)
