81e50330b9b2782c9335b120bd5b636093106b7e
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-12 10:32:11 -0400

    update

--- MainScript.lua
+++ MainScript.lua
@@ -15,6 +15,7 @@ local isfile = isfile or function(file)
 	local suc, res = pcall(function() return readfile(file) end)
 	return suc and res ~= nil
 end
+shared.deffont = Enum.Font.Arial
 local setidentity = syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or function() end
 local getidentity = syn and syn.get_thread_identity or get_thread_identity or getidentity or getthreadidentity or function() return 0 end
 local vapeAssetTable = {
@@ -1800,6 +1801,22 @@ GUISettings.CreateSlider({
 	Max = 100,
 	Default = 10
 })
+
+local fi = {}
+
+for i,v in pairs(Enum.Font:GetEnumItems()) do
+	table.insert(fi, v.Name)
+end
+
+local guifont = GUISettings.CreateDropdown({
+	Name = "GUI Font",
+	List = fi,
+	Function = function(val)
+        shared.deffont = Enum.Font[val]
+    end
+})
+
+
 
 local GUIbind = GUI.CreateGUIBind()
 local teleportConnection = playersService.LocalPlayer.OnTeleport:Connect(function(State)
