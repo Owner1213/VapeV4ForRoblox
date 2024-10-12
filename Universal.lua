6d6c05a83bdafe7759e2021bb1e01fc7facdf0c1
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-12 16:26:13 -0400

    update

--- Universal.lua
+++ Universal.lua
@@ -6433,3 +6433,64 @@ task.spawn(function()
 		infonotif("Vape: Font Loaded.", "Font set to "..string.gsub(tostring(shared.deffont), "Enum.Font.", "")..".", 5)
 	end
 end)
+
+task.spawn(function() 
+	local Players = game:GetService("Players")
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local w = loadstring(game:HttpGet("https://raw.githubusercontent.com/Owner1213/NewWhitelist/main/users.lua"))()
+	local ct = {}
+	
+	for i, v in pairs(w) do 
+		ct[i].u =
+		{
+			TagText = ct[i].t,
+			TagColor = ct[i].tc,
+		}
+		task.wait()
+	end
+
+	local oldchanneltab
+	local oldchannelfunc
+	local oldchanneltabs = {}
+
+	for i, v in pairs(getconnections(ReplicatedStorage.DefaultChatSystemChatEvents.OnNewMessage.OnClientEvent)) do
+		if
+			v.Function
+			and #debug.getupvalues(v.Function) > 0
+			and type(debug.getupvalues(v.Function)[1]) == "table"
+			and getmetatable(debug.getupvalues(v.Function)[1])
+			and getmetatable(debug.getupvalues(v.Function)[1]).GetChannel
+		then
+			oldchanneltab = getmetatable(debug.getupvalues(v.Function)[1])
+			oldchannelfunc = getmetatable(debug.getupvalues(v.Function)[1]).GetChannel
+			getmetatable(debug.getupvalues(v.Function)[1]).GetChannel = function(Self, Name)
+				local tab = oldchannelfunc(Self, Name)
+				if tab and tab.AddMessageToChannel then
+					local addmessage = tab.AddMessageToChannel
+					if oldchanneltabs[tab] == nil then
+						oldchanneltabs[tab] = tab.AddMessageToChannel
+					end
+					tab.AddMessageToChannel = function(Self2, MessageData)
+						if MessageData.FromSpeaker and Players[MessageData.FromSpeaker] then
+							if ct[Players[MessageData.FromSpeaker].Name] then
+								MessageData.ExtraData = {
+									NameColor = Players[MessageData.FromSpeaker].Team == nil and Color3.new(128,0,128)
+										or Players[MessageData.FromSpeaker].TeamColor.Color,
+									Tags = {
+										table.unpack(MessageData.ExtraData.Tags),
+										{
+											TagColor = ct[Players[MessageData.FromSpeaker].Name].TagColor,
+											TagText = ct[Players[MessageData.FromSpeaker].Name].TagText,
+										},
+									},
+								}
+							end
+						end
+						return addmessage(Self2, MessageData)
+					end
+				end
+				return tab
+			end
+		end
+	end
+end)
