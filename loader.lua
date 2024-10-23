c92506a61fb3c6e8c1e679823ed5110061f5911f
Z.. <137589536+Owner1213@users.noreply.github.com>
2024-10-23 18:29:34 -0400

    update

--- loader.lua
+++ loader.lua
@@ -1,46 +1,18 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Owner1213/VapeV4ForRoblox/main/lib.lua", true))()
+if not shared.notid then 
+	shared.notid = nil
+end
 
-shared.FastLoad = false
+if not shared.notied then 
+	shared.notied = nil
+end
 
-local main = lib.CreateScrollGui("main", true)
+if not shared.noties then 
+	shared.noties = nil
+end
 
-local load
-local settings
-
-
-local s = lib.CreateScrollGui("settings", false)
-
-local fastload
+if not shared.deffont then
+	shared.deffont = Enum.Font.GothamMedium
+end
 
 
-load = lib.CreateButton("Load", main, "rbxassetid://117474033428241", function() 
-	lib["MainGui"]:Destroy()
-    shared.notid = nil
-    shared.notied = nil
-    shared.noties = nil
-    shared.deffont = Enum.Font.GothamMedium
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Owner1213/VapeV4ForRoblox/main/NewMainScript.lua", true))()
-end, false)
-
-settings = lib.CreateButton("Settings", main, "rbxassetid://117474033428241", function() 
-	lib["Header"].Text = "Settings"
-	main.Visible = false
-	s.Visible = true
-end, false)
-
-fastload = lib.CreateButton("Fast Load", s, nil, function() 
-	local val = fastload.value
-	if val.Text == "Off" then 
-		val.Text = "On"
-		shared.FastLoad = true
-	else
-		val.Text = "Off"
-		shared.FastLoad = false
-	end
-end, true)
-
-back = lib.CreateButton("Back", s, "rbxassetid://117474033428241", function() 
-	lib["Header"].Text = "Main"
-	main.Visible = true
-	s.Visible = false
-end, false)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Owner1213/VapeV4ForRoblox/main/NewMainScript.lua", true))()
